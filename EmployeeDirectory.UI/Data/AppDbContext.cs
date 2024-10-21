4dd04af33f25b11c0313f5042d3a96398c28dcc2
Aeligeti Greeshma <greeshma425a@gmail.com>
2024-10-21 15:24:57 +0530

    Login Page created

---
+++ EmployeeDirectory.UI/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeDirectory.UI.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeDirectory.UI.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<UserAccount> UserAccounts { get; set; }
+
+    }
+}
---
+++ EmployeeDirectory.UI/Program.cs
@@ -0,0 +1,52 @@
+using EmployeeDirectory.UI.Components;
+using EmployeeDirectory.UI.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeDirectory.UI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
+
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+            {
+                options.Cookie.Name = "auth_token";
+                options.LoginPath = "/login";
+                options.Cookie.MaxAge = TimeSpan.FromMinutes(30);
+                options.AccessDeniedPath = "/access-denied";
+            });
+            builder.Services.AddAuthorization();
+            builder.Services.AddCascadingAuthenticationState();
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+            app.UseAuthentication();    
+            app.UseAuthorization();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
